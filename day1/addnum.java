d2c076286228ec20f81f929e7262f5db0e62634a
dhanisha728-code <dhanisha728@gmail.com>
2026-07-06 13:20:28 +0530

    Add files via upload

---
+++ day1/addnum.java
@@ -0,0 +1,13 @@
+package day1;
+import java.util.Scanner;
+public class addnum {
+ public static void main(String[] args)   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number a:");
+     int a=sc.nextInt();
+     System.out.println("enter number b:");
+     int b=sc.nextInt();
+     int sum=a+b;
+     System.out.println("sum:"+sum);
+ }
+}
---
+++ day1/elsecode.java
@@ -0,0 +1,18 @@
+package day1;
+import java.util.Scanner;
+public class elsecode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative or Zero");
+        }
+
+        sc.close();
+    }
+}
